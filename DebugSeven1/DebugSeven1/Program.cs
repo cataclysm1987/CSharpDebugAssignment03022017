046d83faf982e93c65a821b78cf050837b5e339a
cataclysm1987 <conklin.eric@gmail.com>
2017-03-02 16:27:21 -0500

    add files

---
+++ DebugSeven1/DebugSeven1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Uses DisplayWebAddress method three times
+using static System.Console;
+class DebugSeven1
+{
+   static void Main()
+   {
+      DisplayWebAddress();
+      DisplayWebAddress();
+      DisplayWebAddress();
+   }
+   private static void DisplayWebAddress()
+   {
+      WriteLine("------------------------------");
+      WriteLine("Visit us on the web at:");
+      WriteLine("www.shoppersworldbargains.com");
+      WriteLine("******************************");
+   }
+}
---
+++ DebugSeven3/DebugSeven3/Program.cs
@@ -0,0 +1,47 @@
+﻿// Program gets a quanity ordered from user
+// then determines price and discount based on quantity
+// price per item before discounts is $6.00
+// order 15 or more, get a 20% discount
+// order 10 to 14 - get a 14% discount
+// order 5 to 9, get a 10% discount
+using System;
+using static System.Console;
+class DebugSeven3
+{
+    static void Main()
+    {
+        var quantity = GetQuantity();
+        Console.WriteLine(quantity);
+        var price = CalculatePrice(quantity);
+        WriteLine("Final price for {0} items is {1}.",
+          quantity, price.ToString("c"));
+    }
+
+    static int GetQuantity()
+    {
+        Write("Enter number of items >> ");
+        var quan = Convert.ToInt32(ReadLine());
+        return quan;
+    }
+
+    private static double CalculatePrice(int quantityOrdered)
+    {
+        double PRICE_PER_ITEM = 6.00;
+        double price = 0;
+        double discount = 0;
+        int[] quanLimits = { 0, 5, 10, 15};
+        double[] limits = { 0, 0.10, 0.14, 0.20};
+        for (int x = limits.Length - 1; x >= 0; --x)
+        {
+            if (quantityOrdered >= quanLimits[x])
+            {
+                discount = limits[x];
+                break;
+            }
+        }
+            
+        price = quantityOrdered * PRICE_PER_ITEM;
+        price = price - price * discount;
+        return price;
+    }
+}
